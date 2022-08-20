a311428af021625c79d0e28cfbf69792283ae5ee
Rasmoos <david00gbg@hotmail.com>
2022-08-20 18:12:55 +0200

    0.1.1 First abstraction of Machine creation

--- src/main/java/rasmoos/semirealisticelectricity/blockentites/IronFurnaceBlockEntity.java
+++ src/main/java/rasmoos/semirealisticelectricity/blockentites/IronFurnaceBlockEntity.java
@@ -1,47 +1,17 @@
 package rasmoos.semirealisticelectricity.blockentites;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.game.ClientGamePacketListener;
-import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
-import net.minecraft.world.Containers;
-import net.minecraft.world.MenuProvider;
-import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.items.CapabilityItemHandler;
-import net.minecraftforge.items.IItemHandler;
-import net.minecraftforge.items.ItemStackHandler;
 import org.jetbrains.annotations.Nullable;
 import rasmoos.semirealisticelectricity.screen.IronFurnaceMenu;
 
-import javax.annotation.Nonnull;
-
-public class IronFurnaceBlockEntity extends BlockEntity implements MenuProvider {
+public class IronFurnaceBlockEntity extends MachineBlockEntity {
 
     public static final int NUM_SLOTS = 3;
-
-    private final ItemStackHandler itemHandler = new ItemStackHandler(NUM_SLOTS) {
-        @Override
-        protected void onContentsChanged(int slot) {
-            setChanged();
-        }
-    };
-
-
-
-    private LazyOptional<IItemHandler> lazyItemHandler = LazyOptional.empty();
-
-
 
     public IronFurnaceBlockEntity(BlockPos pWorldPosition, BlockState pBlockState) {
         super(ModBlockEntities.IRON_FURNACE_ENTITY.get(), pWorldPosition, pBlockState);
@@ -49,7 +19,7 @@ public class IronFurnaceBlockEntity extends BlockEntity implements MenuProvider 
 
     @Override
     public Component getDisplayName() {
-        return Component.literal("Gem Cutting Station");
+        return Component.literal("Iron Furnace");
     }
 
     @Nullable
@@ -58,50 +28,7 @@ public class IronFurnaceBlockEntity extends BlockEntity implements MenuProvider 
         return new IronFurnaceMenu(pContainerId, pInventory, this);
     }
 
-    @Nonnull
-    @Override
-    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @javax.annotation.Nullable Direction side) {
-        if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
-            return lazyItemHandler.cast();
-        }
-
-        return super.getCapability(cap, side);
-    }
-
-    @Override
-    public void onLoad() {
-        super.onLoad();
-        lazyItemHandler = LazyOptional.of(() -> itemHandler);
-    }
-
-    @Override
-    public void invalidateCaps()  {
-        super.invalidateCaps();
-        lazyItemHandler.invalidate();
-    }
-
-    @Override
-    protected void saveAdditional(CompoundTag tag) {
-        tag.put("inventory", itemHandler.serializeNBT());
-        super.saveAdditional(tag);
-    }
-
-    @Override
-    public void load(CompoundTag nbt) {
-        super.load(nbt);
-        itemHandler.deserializeNBT(nbt.getCompound("inventory"));
-    }
-
-    public void drops() {
-        SimpleContainer inventory = new SimpleContainer(itemHandler.getSlots());
-        for (int i = 0; i < itemHandler.getSlots(); i++) {
-            inventory.setItem(i, itemHandler.getStackInSlot(i));
-        }
-
-        Containers.dropContents(this.level, this.worldPosition, inventory);
-    }
-
-    public static void tick(Level pLevel, BlockPos pPos, BlockState pState, IronFurnaceBlockEntity pBlockEntity) {
+    public void tick() {
 //        if(pLevel.isClientSide()) {
 //            return;
 //        }
@@ -123,18 +50,8 @@ public class IronFurnaceBlockEntity extends BlockEntity implements MenuProvider 
 //        }
     }
 
-
-    @Nullable
     @Override
-    public Packet<ClientGamePacketListener> getUpdatePacket() {
-        return ClientboundBlockEntityDataPacket.create(this);
-    }
-
-    @Override
-    public CompoundTag getUpdateTag() {
-        CompoundTag compound = saveWithoutMetadata();
-        load(compound);
-
-        return compound;
+    public int getNumberOfSlots() {
+        return NUM_SLOTS;
     }
 }
---
+++ src/main/java/rasmoos/semirealisticelectricity/blocks/MachineBlock.java
@@ -0,0 +1,65 @@
+package rasmoos.semirealisticelectricity.blocks;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.network.NetworkHooks;
+import org.jetbrains.annotations.Nullable;
+import rasmoos.semirealisticelectricity.blockentites.IronFurnaceBlockEntity;
+import rasmoos.semirealisticelectricity.blockentites.MachineBlockEntity;
+
+public abstract class MachineBlock extends BaseEntityBlock {
+    protected MachineBlock(Properties properties) {
+        super(properties);
+    }
+
+
+    @Nullable
+    @Override
+    public <V extends BlockEntity> BlockEntityTicker<V> getTicker(Level level, BlockState state, BlockEntityType<V> blockEntityType) {
+        return (level1, blockPos, blockState, v) -> {
+            if (v instanceof MachineBlockEntity entity)
+                entity.tick();
+        };
+    }
+
+    @Override
+    public RenderShape getRenderShape(BlockState pState) {
+        return RenderShape.MODEL;
+    }
+
+    @Override
+    public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pIsMoving) {
+        if (pState.getBlock() != pNewState.getBlock()) {
+            BlockEntity blockEntity = pLevel.getBlockEntity(pPos);
+            if (blockEntity instanceof MachineBlockEntity entity) {
+                entity.drops();
+            }
+        }
+        super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);
+    }
+
+    @Override
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+        if (!pLevel.isClientSide()) {
+            BlockEntity entity = pLevel.getBlockEntity(pPos);
+            if(entity instanceof MachineBlockEntity blockEntity) {
+                NetworkHooks.openScreen(((ServerPlayer)pPlayer), blockEntity, pPos);
+            } else {
+                throw new IllegalStateException("Our Container provider is missing!");
+            }
+        }
+
+        return InteractionResult.sidedSuccess(pLevel.isClientSide());
+    }
+}
